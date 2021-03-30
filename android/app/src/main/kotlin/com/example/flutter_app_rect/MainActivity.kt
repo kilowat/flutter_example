51492248c423235fbdba7506257e28effa6681f5
kilowat <kadantsev.sergey@gmail.com>
2021-03-30 12:18:25 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_app_rect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_app_rect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// You can read about packages here: https://flutter.io/using-packages/
+import 'package:flutter/material.dart';
+
+// TODO: Import the CategoryRoute widget
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
